fd639b51e0790d6752e49abdbe030ab68d5b7275
AlexandruGabriel02 <70471765+AlexandruGabriel02@users.noreply.github.com>
2023-12-07 01:33:17 +0200

    fixed bug + small adjustment

--- tema5.py
+++ tema5.py
@@ -1,18 +1,27 @@
 import numpy as np
 import random
 
+class Action():
+    LEFT = 0
+    RIGHT = 1
+    DOWN = 2
+    UP = 3
+
+    COUNT = 4
+    arrows = ['←', '→', '↓', '↑']
+
 class State():
     lines = 7
     columns = 10
     finish_pos = (3, 7)
     start_pos = (3, 0)
-    finish_reward = 1000
+    finish_reward = 20
     default_reward = -1
     actions = {
-        0: (0, -1), #left
-        1: (0, 1), #right
-        2: (1, 0), #down
-        3: (-1, 0) #up
+        Action.LEFT: (0, -1),
+        Action.RIGHT: (0, 1),
+        Action.DOWN: (1, 0),
+        Action.UP: (-1, 0)
     }
 
     def __init__(self, agent_pos, wind):
@@ -29,13 +38,11 @@ class State():
         return (next_line, next_col)
 
     def get_next_state(self, action):
-        if self.is_valid_action(action):  #!!!
+        if self.is_valid_action(action):
             next_pos = tuple(np.add(self.agent_pos, self.actions[action]))
             next_pos = State.apply_wind(next_pos, self.wind[self.agent_pos[1]])
             return State(next_pos, self.wind)
         
-        print("Warning: Accessing invalid state")
-        print(f"State: {self.agent_pos}, action: {action}")
         return self 
     
     def is_final_state(self):
@@ -59,7 +66,7 @@ class State():
       
 
 class QLearn():
-    def __init__(self, start_state, alpha=0.7, gamma=0.95, episode_count=1000, min_epsilon=0.1, decay_rate=0.005):
+    def __init__(self, start_state, alpha=0.7, gamma=0.95, episode_count=10000, min_epsilon=0.1, decay_rate=0.005):
         self.start_state = start_state 
         self.alpha = alpha 
         self.gamma = gamma 
@@ -69,19 +76,13 @@ class QLearn():
         
         self.epsilon = 1.0
         
-        self.q_table = np.zeros((State.lines * State.columns, len(State.actions)))
+        self.q_table = np.zeros((State.lines * State.columns, Action.COUNT))
 
     def get_epsilon_greedy_action(self, state):
-        valid_actions = []
-        for action in range(len(State.actions)):
-            if state.is_valid_action(action):
-                valid_actions.append(action)
-        random.shuffle(valid_actions)
-
         if random.uniform(0, 1) <= self.epsilon:
-            rand_action = valid_actions[0]  
+            rand_action = random.randint(0, Action.COUNT - 1)  
             return rand_action
-        return np.argmax(self.q_table[int(state)][valid_actions])
+        return np.argmax(self.q_table[int(state)])
 
     def decrease_epsilon(self):
         if self.epsilon - self.decay_rate >= self.min_epsilon:
@@ -89,13 +90,12 @@ class QLearn():
 
     def train(self):
         for episode in range(self.episode_count):
-            curr_state = self.start_state            
+            curr_state = self.start_state          
             while True:
                 if curr_state.is_final_state():
                     break
-
+                
                 action = self.get_epsilon_greedy_action(curr_state)
-               # print(f"State: {curr_state.agent_pos}, action: {action}")
                 next_state = curr_state.get_next_state(action)
                 
                 self.q_table[int(curr_state)][action] = (1 - self.alpha) * self.q_table[int(curr_state)][action] + self.alpha * (curr_state.get_reward(action) + self.gamma * max(self.q_table[int(next_state)])) 
@@ -106,19 +106,13 @@ class QLearn():
     def print_policy(self):
         for i in range(State.lines):
             for j in range(State.columns):
-                print(np.argmax(self.q_table[i * State.columns + j]), end=' ')
+                action = np.argmax(self.q_table[i * State.columns + j])
+                print(Action.arrows[action], end=' ')
             print()
             
-
-# agent_position = (0, 5)
 wind_effect = [0, 0, 0, 1, 1, 1, 2, 2, 1, 0]
-# state = State(agent_position, wind_effect)
-
-
-# next_state = state.get_next_state(3)
-# print(next_state.agent_pos)
 
 start_state = State((3, 0), wind_effect)
-model = QLearn(start_state, episode_count=2)
+model = QLearn(start_state)
 model.train()
 model.print_policy()
